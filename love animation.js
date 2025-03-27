9206ade61a848273cfe74ace7c3f7126cba07aa3
Ziad-Hossam <ziadhossam5558@gmail.com>
2025-03-27 14:38:21 +0200

    Update love animation.js

--- love animation.js
+++ love animation.js
@@ -321,7 +321,7 @@ const crtLoveTl = () => {
       delay: move,
       fill: el.colHeart,
       x: -64,
-      y: 20,
+      y: 5,
       scale: { 0: 0.95, easing: easingHeart },
       duration: 500,
     })
